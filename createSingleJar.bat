943bb1b3abec07046b65e918ee25ff2f5b61851e
trivo78 <trivo78@gmail.com>
2022-03-02 09:19:19 +0100

    added apache licence

--- createSingleJar.bat
+++ createSingleJar.bat
@@ -1,3 +1,4 @@
+@rem Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
 mvn clean install -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
 mvn clean compile assembly:single -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
 #single jar path--> apache-jena\target\apache-jena-4.3.2-jar-with-dependencies.jar
